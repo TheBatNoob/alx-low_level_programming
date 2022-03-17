dbcfa6958db289ec20e19fcd21ca69878291a428
TheBatNoob <amlabusamuel@gmail.com>
2022-03-17 02:12:00 +0100

    fiii

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,23 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
- * _islower - Shows 1 if the input is a
- * lowercase character. Another cases, shows
- * 0
- *
- * @c: The character in ASCII code
- *
- * Return: 1 for lowercase character. 0 for the rest.
- */
+* _islower - checks for a lowercase character
+*
+* @c: the character to be checked
+*
+* Return: returns 1 if the character is lower case and 0 otherwise
+*/
 int _islower(int c)
 {
-	if (islower(c) != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (islower(c) != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
