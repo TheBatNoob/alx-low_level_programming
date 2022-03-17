d034ae473522bded086ba9118d80f819dd6466d8
TheBatNoob <amlabusamuel@gmail.com>
2022-03-17 03:29:26 +0100

    fiii

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,24 @@
 *
 * Return: Always 0.
 */
-
-int main(int i){ 
-unsigned long fib1 = 0, fib2 = 1, sum;
-
-for (i = 0; i < 4000000; i++) 
+int main(void)
 {
-	sum = fib1 + fib2;
-	printf("%lu", sum);
+unsigned long fib1 = 0, fib2 = 1, fibsum;
+float tot_sum;
 
-	fib1 = fib2;
-	fib2 = sum;
+while (1)
+{
+fibsum = fib1 + fib2;
+if (fibsum > 4000000)
+break;
 
-	if ((i % 2) == 0 )
-		printf("\n");
-	return(0);
+if ((fibsum % 2) == 0)
+tot_sum += fibsum;
+
+fib1 = fib2;
+fib2 = fibsum;
 }
+printf("%.0f\n", tot_sum);
+
+return (0);
 }
